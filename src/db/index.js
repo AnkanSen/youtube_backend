f84e923c721c1c9177a3d9ab2cb2a7c46b5476c7
Ankan Sen <maharajankan@gmail.com>
2024-09-19 16:01:35 +0530

    env changes

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connectDb=async ()=>{
     try {
         console.log(`${process.env.MONGODB_URI}`)
-        const connectionInstance=await mongoose.connect(`mongodb+srv://ankan:ankan123@cluster0.vcx1j.mongodb.net/${DB_NAME}`);
+        const connectionInstance=await mongoose.connect('process.env.MONGODB_URI/${DB_NAME}`);
         console.log(`MongoDb Connected!! ${connectionInstance.connection.host}`);
     } catch (error) {
         console.error(`Error Occured: `,error)
